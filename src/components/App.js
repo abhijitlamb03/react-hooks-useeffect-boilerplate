63f2d72de1f0f481a7f0a8350e6167e82c32239d
shivangbhardwaj2001 <shivangbhardwaj2001@gmail.com>
2022-10-04 11:37:00 +0530

    commiting

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../styles/App.css';
+import { useState, useEffect } from 'react';
+const App = () => {
+ 
+
+
+
+  return (
+    <div>
+      <p id='text' >You clicked this much times</p>
+      <p id='count'>{count}</p>
+      <button id='button' onClick={}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+
+export default App;
